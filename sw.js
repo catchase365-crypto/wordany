358060ad11edacf8505f85e683b02f6cb2537d7e
Catchase <catchase365@gmail.com>
2026-06-17 19:51:52 +0800

    PWA: 智能双线离线固化 sw 更新至 v_1781697112700

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-var C='v_1781550017088';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html'])).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==C&&caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+var C='v_1781697112700';
+var assets = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(C).then(c => c.addAll(assets)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== C && caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res && res.status === 200) {
+          var resClone = res.clone();
+          caches.open(C).then(c => c.put(e.request, resClone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(cached => cached || new Response('Offline', { status: 503 })))
+  );
+});
